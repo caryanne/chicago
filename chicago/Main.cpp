060c6b59020b9c1115a3ae20f1f7edfc4d12f63d
caryanne <adamstevenson121@gmail.com>
2013-11-10 02:38:00 -0500

    physics working, camera roll not working

--- chicago/Main.cpp
+++ chicago/Main.cpp
@@ -23,7 +23,7 @@ GLFWwindow *window;
 
 SceneManager mgr;
 
-bool keyW = false, keyS = false, keyA = false, keyD = false;
+bool keyW = false, keyS = false, keyA = false, keyD = false, keyQ = false, keyE = false;
 
 void setup3d(double w, double h) {
 
@@ -51,23 +51,18 @@ static void keyPress(GLFWwindow* window, int key, int scanCode, int action, int 
 	if(key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
 		glfwSetWindowShouldClose(window, true);
 	
-	if(key == GLFW_KEY_W && action == GLFW_PRESS)
-		keyW = true;
-	if(key == GLFW_KEY_W && action == GLFW_RELEASE)
-		keyW = false;
-	if(key == GLFW_KEY_S && action == GLFW_PRESS)
-		keyS = true;
-	if(key == GLFW_KEY_S && action == GLFW_RELEASE)
-		keyS = false;
-	if(key == GLFW_KEY_A && action == GLFW_PRESS)
-		keyA = true;
-	if(key == GLFW_KEY_A && action == GLFW_RELEASE)
-		keyA = false;
-	if(key == GLFW_KEY_D && action == GLFW_PRESS)
-		keyD = true;
-	if(key == GLFW_KEY_D && action == GLFW_RELEASE)
-		keyD = false;
-
+	if(key == GLFW_KEY_W && action == GLFW_PRESS) keyW = true;
+	if(key == GLFW_KEY_W && action == GLFW_RELEASE)	keyW = false;
+	if(key == GLFW_KEY_S && action == GLFW_PRESS) keyS = true;
+	if(key == GLFW_KEY_S && action == GLFW_RELEASE)	keyS = false;
+	if(key == GLFW_KEY_A && action == GLFW_PRESS) keyA = true;
+	if(key == GLFW_KEY_A && action == GLFW_RELEASE)	keyA = false;
+	if(key == GLFW_KEY_D && action == GLFW_PRESS) keyD = true;
+	if(key == GLFW_KEY_D && action == GLFW_RELEASE)	keyD = false;
+	if(key == GLFW_KEY_Q && action == GLFW_PRESS) keyQ = true;
+	if(key == GLFW_KEY_Q && action == GLFW_RELEASE)	keyQ = false;
+	if(key == GLFW_KEY_E && action == GLFW_PRESS) keyE = true;
+	if(key == GLFW_KEY_E && action == GLFW_RELEASE)	keyE = false;
 
 		
 
@@ -111,7 +106,6 @@ int main() {
 	Entity cubeent = Entity(&cubemesh);
 	SceneNode cube = SceneNode(&cubeent);
 	mgr.getRootNode()->addChild(&cube);
-	cube.setPosition(glm::vec3(0, 4, 4));
 
 	Mesh skymesh = Mesh("skysphere.obj");
 	Entity skyent = Entity(&skymesh);
@@ -135,7 +129,7 @@ int main() {
 	glm::vec3 movSpeed = glm::vec3(0.f);
 	glm::vec3 strafeSpeed = glm::vec3(0.f);
 
-	double xRotSpeed = 0.0, yRotSpeed = 0.0, pitch = 0.0, yaw = 0.0;
+	double xRotSpeed = 0.0, yRotSpeed = 0.0, zRotSpeed = 0.0, pitch = 0.0, yaw = 0.0, roll = 0.0;
 	double mouseX = 0.0, mouseY = 0.0;
 	glfwSetCursorPos(window, width / 2, height / 2);
 	glfwGetCursorPos(window, &mouseX, &mouseY);
@@ -146,24 +140,46 @@ int main() {
 	btSequentialImpulseConstraintSolver* solver = new btSequentialImpulseConstraintSolver();
 	
 	btDiscreteDynamicsWorld* world = new btDiscreteDynamicsWorld(dispatcher, broadphase, solver, collisionConfig);
-	world->setGravity(btVector3(0, -9.8, 0));
+	world->setGravity(btVector3(0, -3, 0));
 
-	btCollisionShape* moduleShape = new btBoxShape(btVector3(10, 1, 10));
+	btCollisionShape* moduleShape = new btBoxShape(btVector3(10, 0.2, 10));
 	btCollisionShape* cubeShape = new btBoxShape(btVector3(1, 1, 1));
+	btCollisionShape* camShape = new btSphereShape(1);
+
+	btVector3 inertia;
+	
 
 	btDefaultMotionState* moduleMotionState =
 		new btDefaultMotionState(btTransform(btQuaternion(0, 0, 0, 1), btVector3(0, 0, 0)));
+	moduleShape->calculateLocalInertia(0, inertia);
 	btRigidBody::btRigidBodyConstructionInfo
-		moduleRigidBodyCI(0, moduleMotionState, moduleShape, btVector3(0, 0, 0));
+		moduleRigidBodyCI(0, moduleMotionState, moduleShape, inertia);
 	btRigidBody* moduleRigidBody = new btRigidBody(moduleRigidBodyCI);
+	world->addRigidBody(moduleRigidBody);
 
 	btDefaultMotionState* cubeMotionState =
-		new btDefaultMotionState(btTransform(btQuaternion(0, 0, 0, 1), btVector3(0, 4, 0)));
-	cubeShape->calculateLocalInertia(1, btVector3(0, 0, 0));
+		new btDefaultMotionState(btTransform(btQuaternion(0, 0, 0, 1), btVector3(0, 2, -7)));
+	cubeShape->calculateLocalInertia(1, inertia);
 	btRigidBody::btRigidBodyConstructionInfo
-		cubeRigidBodyCI(1, cubeMotionState, cubeShape, btVector3(0, 0, 0));
+		cubeRigidBodyCI(1, cubeMotionState, cubeShape, inertia);
 	btRigidBody* cubeRigidBody = new btRigidBody(cubeRigidBodyCI);
+	world->addRigidBody(cubeRigidBody);
+	cubeRigidBody->setActivationState(DISABLE_DEACTIVATION);
+	
+	btDefaultMotionState* camMotionState =
+		new btDefaultMotionState(btTransform(btQuaternion(0, 0, 0, 1), btVector3(0, 0, 0)));
+	camShape->calculateLocalInertia(5, inertia);
+	btRigidBody::btRigidBodyConstructionInfo
+		camRigidBodyCI(5, camMotionState, camShape, inertia);
+	
+	btRigidBody* camRigidBody = new btRigidBody(camRigidBodyCI);
+	//camRigidBody->setCollisionFlags(camRigidBody->getCollisionFlags() | btCollisionObject::CF_KINEMATIC_OBJECT);
+	camRigidBody->setActivationState(DISABLE_DEACTIVATION);
+	world->addRigidBody(camRigidBody);
 
+	cubeRigidBody->setFriction(10);
+	cubeRigidBody->applyTorqueImpulse(btVector3(3, 0, 2));
+	//cubeRigidBody->applyCentralImpulse(btVector3(2, 12, 0));
 
 	printf("%.2f:end initializing systems in %.2fs\n", glfwGetTime(), glfwGetTime() - start);
 	
@@ -189,6 +205,8 @@ int main() {
 		if(keyS) movSpeed = glm::vec3(delta * -7.f);
 		if(keyA) strafeSpeed = glm::vec3(delta * 7.f);
 		if(keyD) strafeSpeed = glm::vec3(delta * -7.f);
+		if(keyQ) zRotSpeed = delta * 100.f;
+		if(keyE) zRotSpeed = delta * -100.f;
 
 		if(abs(dY) > 0.0 && abs(dY)) xRotSpeed += dY * 0.001;
 		if(abs(dX) > 0.0 && abs(dX)) yRotSpeed += dX * 0.001;
@@ -197,12 +215,15 @@ int main() {
 		if(pitch > 80.0) pitch = 80.0;
 		if(pitch < -80.0) pitch = -80.0;
 		yaw += yRotSpeed;
+		roll += zRotSpeed;
+
 		xRotSpeed *= 0.995f;
 		yRotSpeed *= 0.995f;
+		zRotSpeed *= 0.995f;
 
 		mgr.getCamera()->setPosition(mgr.getCamera()->getPosition() - movSpeed * mgr.getCamera()->getDirection());
 		mgr.getCamera()->setPosition(mgr.getCamera()->getPosition() - strafeSpeed * glm::cross(glm::vec3(0,1,0), mgr.getCamera()->getDirection()));
-		mgr.getCamera()->setRotation(glm::vec3(glm::radians(pitch), glm::radians(yaw), 0.f));
+		mgr.getCamera()->setRotation(glm::vec3(glm::radians(pitch), glm::radians(yaw), 0));
 
 		sky.setPosition(mgr.getCamera()->getPosition());
 
@@ -217,10 +238,17 @@ int main() {
 		world->stepSimulation(delta, 10); 
 		btTransform cubeTrans;
 		cubeRigidBody->getMotionState()->getWorldTransform(cubeTrans);
-		cube.setPosition(glm::vec3(cubeTrans.getOrigin().getX(),
-									cubeTrans.getOrigin().getY(),
-									cubeTrans.getOrigin().getZ()));
-	
+		cube.setPosition(glm::vec3(cubeTrans.getOrigin().x(),
+									cubeTrans.getOrigin().y(),
+									cubeTrans.getOrigin().z()));
+		cube.setRotation(glm::quat(cubeTrans.getRotation().w(),
+									cubeTrans.getRotation().x(),
+									cubeTrans.getRotation().y(),
+									cubeTrans.getRotation().z()));
+
+		btTransform camPos = camRigidBody->getCenterOfMassTransform();
+		camPos.setOrigin(btVector3(eye.x, eye.y, eye.z));
+		camRigidBody->setCenterOfMassTransform(camPos);
 
 		mgr.drawScene();
 		
@@ -239,10 +267,18 @@ int main() {
 
 		glfwPollEvents();
 	}
+	world->removeRigidBody(camRigidBody);
+	delete camRigidBody->getMotionState();
+	delete camRigidBody;
+
+	world->removeRigidBody(cubeRigidBody);
+	delete cubeRigidBody->getMotionState();
 	delete cubeRigidBody;
-	delete cubeMotionState;
+
+	world->removeRigidBody(moduleRigidBody);
+	delete moduleRigidBody->getMotionState();
 	delete moduleRigidBody;
-	delete moduleMotionState;
+
 	delete cubeShape;
 	delete moduleShape;
 	delete world;
